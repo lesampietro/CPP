ec8499df5025c5bd3fba506b0ddb0ecb161a5e52
Letícia Sampietro <lsampiet@student.42sp.org.br>
2025-06-02 17:32:34 -0300

    ex00 - refactor

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: lsampiet <lsampiet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 17:07:34 by lsampiet          #+#    #+#             */
-/*   Updated: 2025/06/02 16:54:23 by lsampiet         ###   ########.fr       */
+/*   Updated: 2025/06/02 17:31:41 by lsampiet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,19 @@ int	main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
+	int i = 1;
 	while (--argc)
 	{
-		for (char *p = *++argv; *p; ++p)
-			*p = std::toupper(*p);
-		std::cout << *argv;
+		char *str = argv[i];
+		while (*str)
+		{
+			if (*str >= 'a' || *str <= 'z')
+				*str = std::toupper(*str);
+			std::cout << *str;
+			str++;
+		}
+		i++;
 	}
+	std::cout << std::endl;
 	return (0);
 }
